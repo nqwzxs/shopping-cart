806bde82d398de0d560e73d0d2c67961b9ae9fb5
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-11-02 19:22:53 +0500

    add pages to router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Home from './Home'
+import Shop from './Shop'
+import ErrorPage from './ErrorPage'
+import ShoppingCart from './ShoppingCart'
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'shop', element: <Shop /> },
+        { path: 'cart', element: <ShoppingCart /> },
+      ],
     },
   ])
 

a67cf114cb9fe91a1e92dc7d9729f330d46641f6
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-11-02 19:19:05 +0500

    add function to fetch fake products

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,77 @@
 import '../styles/App.css'
 
 const App = () => {
-  return <></>
+  const [products, setProducts] = useState(null)
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [cartItems, setCartItems] = useState([])
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    setDarkMode(localStorage.getItem('darkMode'))
+
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error('server error')
+        }
+        return res.json()
+      })
+      .then((json) => setProducts(json))
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false))
+  }, [])
+
+  return (
+    <div
+      className={
+        (darkMode ? 'dark ' : '') + 'flex flex-col min-h-screen mx-auto'
+      }
+    >
+      <NavigationBar
+        itemsCount={cartItems.length}
+        switchDarkMode={switchDarkMode}
+      />
+      <div className="flex-1 bg-slate-100 dark:bg-slate-900 dark:text-white p-8">
+        {isLoading && (
+          <div className="mx-auto max-w-min min-h-full">
+            <svg
+              width={48}
+              height={48}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <style>
+                {
+                  '.spinner_ZCsl{animation:spinner_qV4G 1.2s cubic-bezier(0.52,.6,.25,.99) infinite}.spinner_gaIW{animation-delay:.6s}@keyframes spinner_qV4G{0%{r:0;opacity:1}100%{r:11px;opacity:0}}'
+                }
+              </style>
+              <circle className="spinner_ZCsl" cx={12} cy={12} r={0} />
+              <circle
+                className="spinner_ZCsl spinner_gaIW"
+                cx={12}
+                cy={12}
+                r={0}
+              />
+            </svg>
+            {error && <p>{error}</p>}
+          </div>
+        )}
+        {products && (
+          <Outlet
+            context={{
+              products,
+              cartItems,
+              addItemToCart,
+              removeItemFromCart,
+              getQuantity,
+              clearCart,
+            }}
+          />
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default App

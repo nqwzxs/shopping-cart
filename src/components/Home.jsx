d29780fb35bc0644ae12bdd9c8e7b1020c18ecf2
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-11-02 19:22:07 +0500

    add home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col gap-8 place-items-center">
+      <img
+        src="https://media.tenor.com/i4GilKtRp74AAAAd/thrift-shopping.gif"
+        alt="ducks shopping"
+        className="rounded-2xl"
+      />
+      <h1 className="font-bold text-2xl">
+        Welcome to the truly marvelous home page.
+      </h1>
+    </div>
+  )
+}
+
+export default Home
